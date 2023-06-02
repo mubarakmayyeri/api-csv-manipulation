01fd525ecf7cbf178afdeed40398280b251d64dd
Mubarak Mayyeri <mubarakmayyeri@gmail.com>
2023-06-02 19:59:46 +0530

    created basic fastpi app

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get('/')
+async def hello():
+    return {"Hello": "World"}
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
