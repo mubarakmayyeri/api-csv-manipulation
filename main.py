dda44de318c5613a5ce5a231d69068ed02141d4a
Mubarak Mayyeri <mubarakmayyeri@gmail.com>
2023-06-03 16:52:28 +0530

    generated reuslt csv files after processing

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI, UploadFile, File
 import pandas as pd
+import time
+import os
 import uvicorn
 
 app = FastAPI()
+
+# Path for storing result csv files
+base_dir = os.path.dirname(os.path.abspath(__file__))
+result_dir = os.path.join(base_dir, 'results')
+
+if not os.path.exists(result_dir):
+    os.makedirs(result_dir)
+
+results_path = result_dir
 
 @app.post("/process_csv")
 async def process_csv(dataset_1: UploadFile = File(...), dataset_2: UploadFile = File(...)):
@@ -20,21 +31,38 @@ async def process_csv(dataset_1: UploadFile = File(...), dataset_2: UploadFile =
         print("-------------------------------------")
         print(f'Dataset 2: {df2.head()}')
 
-        # Calculate the sum of columns A and B from both CSV files
-        sum_A = df1['A'] + df2['A']
-        sum_B = df1['B'] + df2['B']
+        # Processing the dataframes
+        sum_df1 = df1['A'] + df1['B']
+        sum_df2 = df2['A'] + df2['B']
 
-        # Calculate the product of columns C and D from both CSV files
-        product_C = df1['C'] * df2['C']
-        product_D = df1['D'] * df2['D']
+        diff_df1 = df1['A'] - df1['B']
+        diff_df2 = df2['A'] - df2['B']
 
-        # Create a new DataFrame to store the results
-        result_df = pd.DataFrame({'E': sum_A, 'F': sum_B, 'G': product_C, 'H': product_D})
+        product_df1 = df1['C'] * df1['D']
+        product_df2 = df2['C'] * df2['D']
 
+        div_df1 = df1['C'] / df1['D']
+        div_df2 = df2['C'] / df2['D']
+
+        # Adding time delay of 60s
+        time.sleep(60)
+
+        # Creating resulutant dataframes
+        result_df1 = pd.DataFrame({'E': sum_df1, 'F': diff_df1, 'G': product_df1, 'H': div_df1})
+        result_df2 = pd.DataFrame({'E': sum_df2, 'F': diff_df2, 'G': product_df2, 'H': div_df2})\
+
+        # Printing the reulst dataframes
         print("-------------------------------------")
         print("-------------------------------------")
         print("-------------------------------------")
-        print(f'Results:\n{result_df.head()}')
+        print('Results:')
+        print(f'Ouput_1:\n{result_df1.head()}')
+        print("-------------------------------------")
+        print(f'Ouput_1:\n{result_df2.head()}')
+
+        # Generating output csv files
+        result_1 = result_df1.to_csv(f'{results_path}/result_1.csv', index=False)
+        result_2 = result_df1.to_csv(f'{results_path}/result_2.csv', index=False)
 
         return {"message": "CSV files processed successfully"}
 
